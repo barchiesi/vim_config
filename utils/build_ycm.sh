467ad7e1229b7fc0702b4f058843f56db51f0253
David Barchiesi <david@barchie.si>
2016-04-18 23:27:42 +0200

    - Updated ycm build script and switched to external llvm.

--- utils/build_ycm.sh
+++ utils/build_ycm.sh
@@ -1,8 +1,10 @@
+LLVM_BIN_DIR='~/.vim/llvm'
+
 # Build YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 rm -r /tmp/ycm_buildme
 mkdir -p /tmp/ycm_buildme
 cd /tmp/ycm_buildme
-cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-make -j3 ycm_support_libs
+cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DPATH_TO_LLVM_ROOT=${LLVM_BIN_DIR} . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake --build . --target ycm_core --config Release
